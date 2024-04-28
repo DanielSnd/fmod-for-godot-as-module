e5d65457ef59553a054760fad94bf859446a3c1d
Daniel Snd <daniel@bittentoast.com>
2024-04-28 19:51:02 -0300

    Fixed an issue with android includes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #config.py
 
-AVAILABLE_PLATFORMS = ["windows", "macos", "linuxbsd","web"]
+AVAILABLE_PLATFORMS = ["windows", "android", "macos", "linuxbsd","web"]
 
 def can_build(env, platform):
     return platform in AVAILABLE_PLATFORMS
@@ -8,7 +8,15 @@ def can_build(env, platform):
 def configure(env):
     CORE_LIBRARY_NAME = "fmod"
     STUDIO_LIBRARY_NAME = "fmodstudio"
+
+    lfix = ""
+    debug = False
+    if env["target"] == "template_debug" or env["target"] == "editor":
+        lfix = "L"
+        debug = True
+
     if env["platform"] == "windows":
+        env.Append(CPPPATH=['#../modules/fmodgodot/lib/core/inc/', '#../modules/fmodgodot/lib/studio/inc/'])
         if env["arch"]=="x86":
             env.Append(LIBPATH=["#../modules/fmodgodot/lib/core/lib/x86/"])
             env.Append(LIBPATH=["#../modules/fmodgodot/lib/studio/lib/x86/"])
@@ -22,14 +30,26 @@ def configure(env):
             env.Append(LIBS=["fmod_vc", "fmodstudio_vc"])
         env.Append(CPPDEFINES=["FMOD_WIN"])
     elif env["platform"] == "macos":
+        env.Append(CPPPATH=['#../modules/fmodgodot/lib/core/inc/', '#../modules/fmodgodot/lib/studio/inc/'])
         env.Append(LIBPATH=["#../modules/fmodgodot/lib/core/lib/", "#../modules/fmodgodot/lib/studio/lib/"])
         env.Append(LIBS=["fmod", "fmodstudio"])
         env.Append(CPPDEFINES=["FMOD_OSX"])
     elif env["platform"] == "android":
-        env.Append(LIBPATH=["#../modules/fmodgodot/lib/core/lib/arm64-v8a/", "#../modules/fmodgodot/lib/studio/lib/arm64-v8a/"])
-        env.Append(LIBS=["libfmod", "libfmodstudio"])
+        libfmod = 'libfmod%s' % lfix
+        libfmodstudio = 'libfmodstudio%s' % lfix
+        fmod_info_table = {
+            "armv7": "armeabi-v7a/",
+            "arm64": "arm64-v8a/",
+            "x86": "x86/",
+            "x86_64": "x86_64/"
+        }
+        arch_dir = fmod_info_table[env["arch"]]
+        env.Append(CPPPATH=['#../modules/fmodgodot/lib/core/android/inc/', '#../modules/fmodgodot/lib/studio/android/inc/'])
+        env.Append(LIBPATH=['#../modules/fmodgodot/lib/core/android/lib/'+ arch_dir, '#../modules/fmodgodot/lib/studio/android/lib/' + arch_dir])
+        env.Append(LIBS=[libfmod, libfmodstudio])
         env.Append(CPPDEFINES=["FMOD_ANDROID"])
     elif env["platform"] == "linuxbsd":
+        env.Append(CPPPATH=['#../modules/fmodgodot/lib/core/inc/', '#../modules/fmodgodot/lib/studio/inc/'])
         env.Append(LIBPATH=["#../modules/fmodgodot/lib/core/lib/x86_64/"])
         env.Append(LIBPATH=["#../modules/fmodgodot/lib/studio/lib/x86_64/"])
         env.Append(LIBS=["libfmod","libfmodstudio"])
@@ -37,5 +57,6 @@ def configure(env):
         env.Append(CPPDEFINES=["FMOD_LINUX"])
     elif env["platform"] == "web":
         env.Append(CPPDEFINES=["FMOD_WEB"])
+        env.Append(CPPPATH=['#../modules/fmodgodot/lib/core/inc/', '#../modules/fmodgodot/lib/studio/inc/'])
         env.AddJSLibraries(["#../modules/fmodgodot/lib/core/lib/upstream/js/fmod_reduced.js","#../modules/fmodgodot/lib/studio/lib/upstream/js/fmodstudio.js"])
         env.AddJSExterns(["#../modules/fmodgodot/lib/core/lib/upstream/js/fmod_reduced.js.mem","#../modules/fmodgodot/lib/studio/lib/upstream/js/fmodstudio.js.mem"])
